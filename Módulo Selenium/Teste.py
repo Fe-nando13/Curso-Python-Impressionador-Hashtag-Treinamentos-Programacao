2b33ecbef495f09b408fe0212a58fafb6f8445af
Erick <erickspavanii@gmail.com>
2022-04-03 22:43:32 -0300

    Added files

--- Módulo Selenium/Teste.py
+++ Módulo Selenium/Teste.py
@@ -1,5 +1,16 @@
 from selenium import webdriver
+import time
 
-driver = webdriver.Chrome()
+# Pra abrir o navegador em segundo plano
+chrome_options = webdriver.ChromeOptions()
+# Pra abrir o navegador em segundo plano
+chrome_options.add_argument('headless')
+driver = webdriver.Chrome(options=chrome_options)
+driver.set_window_size(1024, 600)
+driver.maximize_window()
 driver.get("https://erick-pavani.github.io/")
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="navbarNav"]/div/a[5]').click()
+time.sleep(2)
+driver.find_element_by_class_name("Linkedin").click()
 print(driver.title)
